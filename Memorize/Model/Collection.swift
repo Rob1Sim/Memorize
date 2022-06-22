202c761345c9ace9f6d7c179a962261fddb62db3
robis <robin.le.dev@gmail.com>
2022-06-22 15:16:34 +0200

    Ajout de commentaires au méthode de collection

--- Memorize/Model/Collection.swift
+++ Memorize/Model/Collection.swift
@@ -18,20 +18,35 @@ struct Collection{
         }
     }
     
-    
+    /**
+                Recupère l'id de la carte
+                return Int id
+     **/
     private func getId()->Int{
         return self.id
         
     }
     
+    /**
+                Recupère le nom de la carte
+                return String name
+     **/
     private func getName()->String{
         return self.name
     }
     
+    /**
+                Recupère les cartes présente dans la collection
+                return Array<card> cards
+     **/
     private func getCards()->Array<Card>{
         return self.cards
     }
     
+    /**
+                Modifie le nom avec le nom passé en paramètre
+                String n Nom
+     **/
     private mutating func setName(value n:String)->Void{
         self.name = n
     }
